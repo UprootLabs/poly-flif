1e3a80a5c433834b3ac23dc2c1107dfba0f3e5cb
hrj <harshad.rj@gmail.com>
2015-10-05 11:26:19 +0530

    png comparison

---
+++ imgInfo.js
@@ -0,0 +1,23 @@
+var imgInfos = [
+  {},
+  { name: '1_webp_ll',
+    imgSize: { width: 400, height: 301},
+    fileSizes: { flif: 78108, png: 150890}
+  },
+  { name: '2_webp_ll',
+    imgSize: { width: 386, height: 395},
+    fileSizes: { flif: 28872, png: 68067}
+  },
+  { name: '3_webp_ll',
+    imgSize: { width: 800, height: 600},
+    fileSizes: { flif: 157986, png: 290118}
+  },
+  { name: 'monkey',
+    imgSize: { width: 1132, height: 849},
+    fileSizes: { flif: 1193044, png: 1943037}
+  },
+  { name: 'watch_by8',
+    imgSize: { width: 497, height: 331},
+    fileSizes: { flif: 171414, png: 269331}
+  },
+];
