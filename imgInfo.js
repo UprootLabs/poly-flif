fb92f5a8ee68140287614745662268c92dc21aa9
hrj <harshad.rj@gmail.com>
2015-10-08 12:11:33 +0530

    updated png sizes

--- imgInfo.js
+++ imgInfo.js
@@ -2,17 +2,17 @@ var imgInfos = [
   {},
   { name: '1_webp_ll',
     fileSizes: { flif: 78108
-      , png: 150890}, 
+      , png: 149962},
     imgSize: { width: 400, height: 301},
   },
   { name: '2_webp_ll',
     fileSizes: { flif: 28872
-      , png: 68067}, 
+      , png: 62327},
     imgSize: { width: 386, height: 395},
   },
   { name: '3_webp_ll',
     fileSizes: { flif: 157986
-      , png: 290118}, 
+      , png: 289737},
     imgSize: { width: 800, height: 600},
   },
   { name: 'clock',
@@ -21,7 +21,7 @@ var imgInfos = [
   },
   { name: 'monkey',
     fileSizes: { flif: 1193044
-      , png: 1943037}, 
+      , png: 1913870},
     imgSize: { width: 1132, height: 849},
   },
   { name: 'pyani',
@@ -42,7 +42,7 @@ var imgInfos = [
   },
   { name: 'watch_by8',
     fileSizes: { flif: 171414
-      , png: 269331}, 
+      , png: 269025},
     imgSize: { width: 497, height: 331},
   },
 ];
