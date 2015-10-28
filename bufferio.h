81ae535d5d38464b933974226661919a04c441dd
hrj <harshad.rj@gmail.com>
2015-10-28 14:21:05 +0530

    fixed value of EOS

--- bufferio.h
+++ bufferio.h
@@ -24,7 +24,7 @@ private:
   void operator=(BufferIO&&) {}
 
 public:
-  const int EOS = 1;
+  const int EOS = -1;
 
   BufferIO(int abufId, const char *aname, const int truncatePercent) : bufId(abufId), name(aname) {
     size = jsBufGetSize(bufId);
