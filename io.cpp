b67eac555f593c96331a7a68452248d63e225164
hrj <harshad.rj@gmail.com>
2015-10-28 14:24:21 +0530

    Add option to DEBUG print

--- io.cpp
+++ io.cpp
@@ -1,12 +1,11 @@
-#if 0
+#ifdef DEBUG
 #include <stdio.h>
 #include <stdarg.h>
 #include "io.hpp"
 #endif
 
 void e_printf(const char *format, ...) {
-// TODO: Consider implementing e_printf
-#if 0
+#ifdef DEBUG
     va_list args;
     va_start(args, format);
     vfprintf(stderr, format, args);
@@ -15,7 +14,12 @@ void e_printf(const char *format, ...) {
 #endif
 }
 
+#ifdef DEBUG
+static int verbosity = 3;
+#else
 static int verbosity = 1;
+#endif
+
 void increase_verbosity() {
     verbosity ++;
 }
@@ -24,7 +28,7 @@ int get_verbosity() {
     return verbosity;
 }
 
-#if 0
+#ifdef DEBUG
 void v_printf(const int v, const char *format, ...) {
     if (verbosity < v) return;
     va_list args;
--- io.hpp
+++ io.hpp
@@ -1,7 +1,12 @@
 #pragma once
 
 void e_printf(const char *format, ...);
+
+#ifdef DEBUG
+void v_printf(const int v, const char *format, ...);
+#else
 inline void v_printf(const int v, const char *format, ...) {}
+#endif
 
 void increase_verbosity();
 int get_verbosity();
