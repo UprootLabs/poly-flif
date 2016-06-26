322140bbec0a4bfe91b229a4764b7b5b9eb7d15d
hrj <harshad.rj@gmail.com>
2016-06-26 19:48:11 +0530

    poly fill for toBlob()

--- src/flif-post.js
+++ src/flif-post.js
@@ -151,6 +151,24 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   }
 
+  // Poly-fill for toBlob, inspired by MDN docs
+  if (!HTMLCanvasElement.prototype.toBlob) {
+    Object.defineProperty(HTMLCanvasElement.prototype, 'toBlob', {
+      value: function (callback, type, quality) {
+
+        var binStr = atob(this.toDataURL(type, quality).split(',')[1] ),
+            len = binStr.length,
+            arr = new Uint8Array(len);
+
+        for (var i=0; i<len; i++ ) {
+          arr[i] = binStr.charCodeAt(i);
+        }
+
+        callback( new Blob( [arr], {type: type || 'image/png'} ) );
+      }
+    });
+  }
+
   var canvasElements = document.querySelectorAll("canvas[data-polyflif-src]");
   for (var i = 0; i < canvasElements.length; i++) {
     process(canvasElements[i]);
