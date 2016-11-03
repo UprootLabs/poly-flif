54612b90714a890ddd3fafd05296e717f40c5eac
hrj <harshad.rj@gmail.com>
2016-11-03 22:43:43 +0530

    update imginfo

--- src/em-out/imgInfo.js
+++ src/em-out/imgInfo.js
@@ -1,87 +1,93 @@
 var imgInfos = [
   {},
   { name: '1_webp_ll',
-    fileSizes: { flif: 78641
+    fileSizes: { flif: 77723
       , png: 149962
      },
     imgSize: { width: 400, height: 301},
   },
   { name: '2_webp_ll',
-    fileSizes: { flif: 26345
+    fileSizes: { flif: 25832
       , png: 62327
      },
     imgSize: { width: 386, height: 395},
   },
   { name: '3_webp_ll',
-    fileSizes: { flif: 157492
+    fileSizes: { flif: 150231
       , png: 289737
      },
     imgSize: { width: 800, height: 600},
   },
   { name: '5_webp_ll',
-    fileSizes: { flif: 97704
+    fileSizes: { flif: 95833
       , png: 155461
      },
     imgSize: { width: 300, height: 300},
   },
   { name: 'clock',
-    fileSizes: { flif: 22290
+    fileSizes: { flif: 21791
      },
   },
   { name: 'kodim07',
-    fileSizes: { flif: 367569
+    fileSizes: { flif: 356699
       , png: 629128
       , jpg: 246495
      },
     imgSize: { width: 768, height: 512},
   },
   { name: 'kodim08',
-    fileSizes: { flif: 512668
+    fileSizes: { flif: 503707
       , png: 854343
       , jpg: 361373
      },
     imgSize: { width: 768, height: 512},
   },
   { name: 'kodim12',
-    fileSizes: { flif: 391263
+    fileSizes: { flif: 386269
       , png: 590626
       , jpg: 247921
      },
     imgSize: { width: 768, height: 512},
   },
   { name: 'kodim13',
-    fileSizes: { flif: 564753
+    fileSizes: { flif: 545751
       , png: 880638
       , jpg: 381372
      },
     imgSize: { width: 768, height: 512},
   },
   { name: 'monkey',
-    fileSizes: { flif: 1203056
+    fileSizes: { flif: 1183456
       , png: 1913870
       , jpg: 708357
      },
     imgSize: { width: 1132, height: 849},
   },
   { name: 'pyani',
-    fileSizes: { flif: 581107
+    fileSizes: { flif: 558023
      },
   },
   { name: 'spinfox',
-    fileSizes: { flif: 372480
+    fileSizes: { flif: 355168
      },
   },
   { name: 'tea',
-    fileSizes: { flif: 481916
+    fileSizes: { flif: 458953
      },
   },
   { name: 'train',
-    fileSizes: { flif: 1246687
+    fileSizes: { flif: 1179632
      },
   },
   { name: 'watch_by8',
-    fileSizes: { flif: 172355
+    fileSizes: { flif: 162725
       , png: 269025
+     },
+    imgSize: { width: 497, height: 331},
+  },
+  { name: 'watch_by8_gray',
+    fileSizes: { flif: 61218
+      , png: 88954
      },
     imgSize: { width: 497, height: 331},
   },
